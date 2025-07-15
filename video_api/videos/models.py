2ed8694de0acdbaaefc47167ef0a9c339b03d129
Maniakalochka <prochorwolochoff@gmail.com>
2025-07-15 23:40:01 +0300

    Create first model ("Video")

--- video_api/videos/models.py
+++ video_api/videos/models.py
@@ -4,11 +4,19 @@ from django.conf import settings
 
 class Video(models.Model):
     owner = models.ForeignKey(
-            settings.AUTH_USER_MODEL,
-            on_delete=models.CASCADE,
-            related_name="videos"
-        )
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="videos"
+    )
     name = models.CharField(max_length=255, verbose_name="Название")
     is_published = models.BooleanField(default=False, verbose_name="Опубликовано")  # type: ignore
     total_likes = models.PositiveIntegerField(default=0, verbose_name="Всего лайков")  # type: ignore
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "Видео"
+        verbose_name_plural = "Видео"
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return str(self.name)
